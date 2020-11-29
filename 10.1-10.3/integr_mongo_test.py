ab93ffb4a9e4e9c7e8d8f48b59dcd607db9fc4b6
RakhimovEmil <69158088+RakhimovEmil@users.noreply.github.com>
2020-11-29 14:51:11 +0300

    Update integr_mongo_test.py

--- 10.1-10.3/integr_mongo_test.py
+++ 10.1-10.3/integr_mongo_test.py
@@ -6,7 +6,7 @@ from repository import Mango
 class MongoTestCase(unittest.TestCase):
     def setUp(self):
         mongo_client = pymongo.MongoClient(host='0.0.0.0', port=27017)
-        self.table = MongoRep(mongo_client)
+        self.table = Mango(mongo_client)
         os.system('docker run --rm --detach --name emils-mongo-test  --publish 27017:27017 mongo')
 
     def test_get_returns_value_from_cache(self):
